757472def6258e856ebdad2d9c6954359feb2a90
Radek Poleski <radek.poleski@gmail.com>
2017-02-20 18:00:21 -0500

    many changes in multipletpf.py

--- code/multipletpf.py
+++ code/multipletpf.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+import tpfdata
 
 
 class MultipleTpf(object):
@@ -5,45 +8,58 @@ class MultipleTpf(object):
 
     def __init__(self):
         self._tpfs = set()
-        self._pixel_column_list = []
-        self._pixel_row_list = []
-        self._median_list = []
-        self._pixel_flux_list = []
+        self._epic_ids = set()
         self._predictor_epoch_mask = None
+        self._campaign = None
 
     def add_tpf_data(self, tpf_data):
         """add one more instance of TpfData"""
+        if not isinstance(tpf_data, tpfdata.TpfData):
+            raise ValueError('Ooops... MultipleTpf.add_tpf_data() requires input that is an instance of TpfData class')
+        if tpf_data.epic_id in self._epic_ids:
+            return
+        if self._campaign is None:
+            self._campaign = tpf_data.campaign
+        else:
+            if self._campaign != tpf_data.campaign:
+                msg = 'MultipleTpf.add_tpf_data() cannot add data from a different campaign ({:} and {:})'
+                raise ValueError(msg.format(self._campaign, tpf_data.campaign))
+            
         self._tpfs.add(tpf_data)
-        self._pixel_column_list.append(tpf_data.pixel_column)
-        self._pixel_row_list.append(tpf_data.pixel_row)
-        self._median_list.append(tpf_data.median)
-        self._pixel_flux_list.append(tpf_data.flux)
+        self._epic_ids.add(tpf_data.epic_id)
         if self._predictor_epoch_mask is None:
             self._predictor_epoch_mask = np.ones_like(tpf_data.epoch_mask, dtype=bool)
         self._predictor_epoch_mask &= tpf_data.epoch_mask
-
-    @property
-    def pixel_column(self):
-        """table with all concatenated pixel columns"""
-        return np.concatenate(self._pixel_column_list, axis=0).astype(int)
-
-    @property
-    def pixel_row(self):
-        """table with all concatenated pixel rows"""
-        return np.concatenate(self._pixel_row_list, axis=0).astype(int)
-
-    @property
-    def pixel_median(self):
-        """table with all concatenated median values"""
-        return np.concatenate(self._median_list, axis=0)
-
-    @property
-    def pixel_flux(self):
-        """table with all concatenated pixel flux values"""
-        return np.concatenate(self._pixel_flux_list, axis=1)
 
     @property
     def predictor_epoch_mask(self):
         """predictor epoch mask"""
         return self._predictor_epoch_mask
 
+    def add_tpf_data_from_epic_list(self, epic_id_list, campaign):
+        """for each epic_id in the list, construct TPF object and add it to the set"""
+        for epic_id in epic_id_list:
+            new_tpf = tpfdata.TpfData(epic_id=epic_id, campaign=campaign)
+            self.add_tpf_data(new_tpf)
+            
+    def add_tpf_data_from_epic_list_in_file(self, epic_id_list_file, campaign):
+        """read data from file and apply add_tpf_data_from_epic_list()"""
+        with open(epic_id_list_file) as list_file:
+            epic_id_list = [int(line) for line in list_file.readlines()]
+        self.add_tpf_data_from_epic_list(epic_id_list=epic_id_list, campaign=campaign)
+
+
+if __name__ == '__main__':
+    directory = 'tpf/'
+    campaign = 92 
+    tpfs_epic_list_file = "../test/output/1-epic.dat"
+    out_file = '1-predictor_epoch_mask.dat'
+    
+    tpfdata.TpfData.directory = directory
+    
+    tpf_set = MultipleTpf()
+    tpf_set.add_tpf_data_from_epic_list_in_file(tpfs_epic_list_file, campaign=campaign)
+    predictor_epoch_mask = tpf_set.predictor_epoch_mask
+
+    np.savetxt(out_file, predictor_epoch_mask, fmt='%r')
+    
