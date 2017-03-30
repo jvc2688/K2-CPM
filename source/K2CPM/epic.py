f966e22d2deb97f682d17786d6a79d680e0baa84
Radek Poleski <radek.poleski@gmail.com>
2017-03-30 13:37:12 -0400

    epic.py compatible with python2 and python3

--- source/K2CPM/epic.py
+++ source/K2CPM/epic.py
@@ -4,6 +4,8 @@ import requests
 import json
 import urllib
 import os
+import sys
+
 
 def get_tpfs(ra,dec,r,camp, channel):
     url = 'https://archive.stsci.edu/k2/data_search/search.php'
@@ -14,13 +16,20 @@ def get_tpfs(ra,dec,r,camp, channel):
     return list(set(r_list))
 
 def load_tpf(ID, camp, dire):
+    """check if the TPF file exists and download it if not"""
     ID = int(ID)
+    file_name = 'ktwo{0:d}-c{1:d}_lpd-targ.fits.gz'.format(ID, camp)
+    destination = os.path.join(dire, file_name)
+    if os.path.isfile(destination): # If the files is there, then there is nothing else to do, otherwise we need to download the data.
+        return    
+        
     d3 = ID % 1000
     d2 = ID % 100000
     d1 = ID-d2
     d2 -= d3
-    file_name = 'ktwo{0:d}-c{1:d}_lpd-targ.fits.gz'.format(ID, camp)
-    if not os.path.isfile(dire+'/'+file_name):
-        load_url = 'http://archive.stsci.edu/missions/k2/target_pixel_files/c{0:d}/{1:d}/{2:05d}/ktwo{3:d}-c{4:d}_lpd-targ.fits.gz'.format(camp, d1, d2, ID, camp)
-        tpf_file = urllib.URLopener()
-        tpf_file.retrieve(load_url, dire+'/'+file_name)
+    fmt = 'http://archive.stsci.edu/missions/k2/target_pixel_files/c{0:d}/{1:d}/{2:05d}/ktwo{3:d}-c{4:d}_lpd-targ.fits.gz'
+    load_url = fmt.format(camp, d1, d2, ID, camp)
+    if (sys.version_info > (3, 0)):
+        urllib.request.urlretrieve(load_url, destination)
+    else:
+        urllib.urlretrieve(load_url, destination)
