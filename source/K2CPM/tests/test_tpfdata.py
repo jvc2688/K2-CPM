d381b4d4a07f307bc75e87702a904665652a491f
Radek Poleski <radek.poleski@gmail.com>
2017-05-31 14:30:30 -0400

    minor edits in test_tpfdata.py

--- source/K2CPM/tests/test_tpfdata.py
+++ source/K2CPM/tests/test_tpfdata.py
@@ -4,11 +4,12 @@ from K2CPM import tpfdata
 
 
 def test_check_pixel_in_tpf():
+    """testing check_pixel_in_tpf() method of TpfData"""
     campaign = 92
     channel = 31
-    pix_y = 670#265
-    pix_x = 883#494
-    epic_id = 200071074#200069761
+    pix_y = 670
+    pix_x = 883
+    epic_id = 200071074
     tpf_dir = 'tpf/'
     
     tpfdata.TpfData.directory = tpf_dir
